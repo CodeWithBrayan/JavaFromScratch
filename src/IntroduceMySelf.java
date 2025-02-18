91f3ec6a43182301b507f17d83bbf9fd41e55a37
Brayan Silva <brayan_silva82151@elpoli.edu.co>
2025-02-18 01:40:27 -0500

    feat: Introducing myself

---
+++ src/IntroduceMySelf.java
@@ -0,0 +1,8 @@
+public class IntroduceMySelf {
+
+    public static void main(String[] args) {
+        System.out.println("Name: Brayan Alexis Silva Ramirez");
+        System.out.println("Age: 28");
+        System.out.println("Country: Colombia");
+    }
+}
