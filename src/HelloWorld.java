f912cdb43d54ca6bdbe200d0c4ee228d855c2123
Brayan Silva <brayan_silva82151@elpoli.edu.co>
2025-02-18 01:32:33 -0500

    feat: add Hello World command line

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    //Add principal method (Main)
+    public static void main(String[] args) {
+        System.out.println("Hello World with Java");
+        System.out.println("Greetings");
+    }
+
+}
